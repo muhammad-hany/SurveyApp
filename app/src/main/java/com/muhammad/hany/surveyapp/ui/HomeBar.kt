5723561a21b315e587902313b8d73907425a72c6
muhammad <mohammedeletreby@gmail.com>
2025-02-14 14:33:16 +0200

    used system TopAppBar instead of surface

--- app/src/main/java/com/muhammad/hany/surveyapp/ui/HomeBar.kt
+++ app/src/main/java/com/muhammad/hany/surveyapp/ui/HomeBar.kt
@@ -1,13 +1,8 @@
 package com.muhammad.hany.surveyapp.ui
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
@@ -15,42 +10,41 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.muhammad.hany.surveyapp.R
 import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeBar(
     pagerState: PagerState,
-    modifier: Modifier = Modifier,
 ) {
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.fillMaxWidth(),
-        contentColor = MaterialTheme.colorScheme.onPrimary
-    ) {
-        val coroutineScope = rememberCoroutineScope()
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .padding(horizontal = 4.dp)
-                .windowInsetsPadding(TopAppBarDefaults.windowInsets)
-                .height(dimensionResource(R.dimen.top_bar_height))
-        ) {
-            IconButton(onClick = { }) {
+    val navController = LOCAL_NAVIGATOR.current
+    val coroutineScope = rememberCoroutineScope()
+    TopAppBar(
+        title = { Text(
+            "Question ${pagerState.currentPage + 1}/${pagerState.pageCount}",
+            maxLines = 1,
+            fontSize = 20.sp,
+            modifier = Modifier.fillMaxWidth()
+        ) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowLeft,
                     contentDescription = stringResource(R.string.menu),
@@ -58,9 +52,8 @@ fun HomeBar(
                     tint = Color.White
                 )
             }
-            Spacer(Modifier.weight(1F))
-            Text(text = "Question ${pagerState.currentPage + 1}/${pagerState.pageCount}")
-            Spacer(Modifier.weight(1F))
+        },
+        actions = {
             Row {
                 TextButton(onClick = {
                     coroutineScope.launch {
@@ -79,5 +72,5 @@ fun HomeBar(
                 }
             }
         }
-    }
+    )
 }
