af91caadf9eb13c329d604f0bd57b56256cde479
muhammad <mohammedeletreby@gmail.com>
2025-02-15 14:51:02 +0200

    changed transition animations

--- app/src/main/java/com/muhammad/hany/surveyapp/ui/MainActivity.kt
+++ app/src/main/java/com/muhammad/hany/surveyapp/ui/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.muhammad.hany.surveyapp.ui.screens.HomeScreen
-import com.muhammad.hany.surveyapp.ui.screens.SurveyScreen
 import com.muhammad.hany.surveyapp.ui.navigation.Home
 import com.muhammad.hany.surveyapp.ui.navigation.Survey
+import com.muhammad.hany.surveyapp.ui.navigation.SurveyAppNavigation
+import com.muhammad.hany.surveyapp.ui.screens.HomeScreen
+import com.muhammad.hany.surveyapp.ui.screens.SurveyScreen
 import com.muhammad.hany.surveyapp.ui.theme.SurveyAppTheme
 import com.xm.tka.ui.ViewStore.Companion.view
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -38,7 +38,7 @@ fun SurveyApp(viewModel: HomeViewModel) {
     CompositionLocalProvider(
         LOCAL_NAVIGATOR provides navController
     ) {
-        NavHost(navController = navController, startDestination = Home) {
+        SurveyAppNavigation(navController = navController) {
             composable(Home) { HomeScreen(viewModel.store.view()) }
             composable(Survey) { SurveyScreen(viewModel.store.view()) }
         }
