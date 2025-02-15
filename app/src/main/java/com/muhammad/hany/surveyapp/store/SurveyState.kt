9ece6ce680d40825ee1af8f958e7bd27ae3a0149
muhammad <mohammedeletreby@gmail.com>
2025-02-15 20:55:16 +0200

    removed home screen error from survey state

--- app/src/main/java/com/muhammad/hany/surveyapp/store/SurveyState.kt
+++ app/src/main/java/com/muhammad/hany/surveyapp/store/SurveyState.kt
@@ -5,7 +5,6 @@ import com.muhammad.hany.surveyapp.ui.model.SurveyQuestion
 data class SurveyState(
     val surveyQuestions: List<SurveyQuestion> = emptyList(),
     val isLoading: Boolean = false,
-    val error: String? = null
 ) {
     val answeredQuestionsCount = surveyQuestions.count { it.answer != null && it.successfullyAnswered }
 }
